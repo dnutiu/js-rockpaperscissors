0d04c936c6411c70b232944757562ea3d5089cfd
Nutiu Denis <nutiudenis@hotmail.com>
2015-08-01 13:25:33 +0300

    writting moves skeleton

---
+++ javascripts/moves.js
@@ -0,0 +1,21 @@
+function Moves() {
+	this.isBetterThan = [];
+	this.isWorseThan = [];
+}
+
+(function() {
+	this.greaterThan = function() {
+
+	};
+	this.lessThan = function() {
+
+	};
+	this.equal = function() {
+
+	};
+}).call(Moves.prototype);
+
+console.log("====moves.js====")
+console.log(Moves.name);
+console.log('r' in ['a','b','c']);
+console.log("====END====")
